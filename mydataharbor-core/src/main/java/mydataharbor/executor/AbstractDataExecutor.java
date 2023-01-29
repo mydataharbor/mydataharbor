1fd55d62fc13f5dd6cb714fffa32feecc72066d2
xulang <xulang@doba.com>
2023-01-29 10:45:16 +0800

    切换版本号，代码格式调整

--- mydataharbor-core/src/main/java/mydataharbor/executor/AbstractDataExecutor.java
+++ mydataharbor-core/src/main/java/mydataharbor/executor/AbstractDataExecutor.java
@@ -788,7 +788,7 @@ public abstract class AbstractDataExecutor<T, P extends IProtocolData, R, S exte
    */
   @Override
   public void run() {
-      TaskStorageThreadLocal.set(taskStorage);
+    TaskStorageThreadLocal.set(taskStorage);
     end = false;
     taskMonitor.setEnd(end);
     if (!suspend)
@@ -808,29 +808,22 @@ public abstract class AbstractDataExecutor<T, P extends IProtocolData, R, S exte
       taskMonitor.setTotal(dataSource.total());
       while (run) {
         while (suspend) {
-          if (!run) {
-            //允许暂停时被结束
-            break;
-          }
+          if (!run) break;//允许暂停时被结束
           taskMonitor.setLastRunTime(System.currentTimeMillis());
           try {
-            Thread.sleep(500);
-          } catch (InterruptedException e) {
-            log.error("暂停被打断");
+              Thread.sleep(500);
+          }
+          catch (InterruptedException e) {
+              log.error("暂停被打断");
           }
         }
-        if (!run) {
-          break;
-        }
+        if (!run) break;
         long startTime = System.currentTimeMillis();
         try {
             Boolean isWrite = doRun(dataSource, protocolDataConverter, checker, dataConverter, sink);
-            if(isWrite)
-                taskMonitor.useTimeIncrease(System.currentTimeMillis() - startTime);
+            if(isWrite) taskMonitor.useTimeIncrease(System.currentTimeMillis() - startTime);
         } catch (TheEndException e) {
-          //数据拉取完毕
-          //跳出循环
-          break;
+            break;//数据拉取完毕 跳出循环
         } finally {
           taskMonitor.setLastRunTime(System.currentTimeMillis());
         }
@@ -849,11 +842,8 @@ public abstract class AbstractDataExecutor<T, P extends IProtocolData, R, S exte
     } finally {
       end = true;
       taskMonitor.setEnd(end);
-      if (run)
-        safeListenerRun(this::close);
-      if (forkJoinPool != null) {
-        forkJoinPool.shutdown();
-      }
+      if (run) safeListenerRun(this::close);
+      if (forkJoinPool != null) forkJoinPool.shutdown();
       log.info("{}该线程结束！", getName());
     }
 
@@ -890,9 +880,7 @@ public abstract class AbstractDataExecutor<T, P extends IProtocolData, R, S exte
           return false;
       }
     boolean empty = !tRecordsIterable.iterator().hasNext();
-    if (empty) {
-      return false;
-    }
+    if (empty) return false;
     taskMonitor.tRecordUseTimeIncrease(System.currentTimeMillis() - tRecordUseTimeStart);
     rollbackUnit = new ConcurrentHashMap<>();
     log.info("原始数据源数据：{}", tRecordsIterable);
@@ -1268,7 +1256,7 @@ public abstract class AbstractDataExecutor<T, P extends IProtocolData, R, S exte
     return taskMonitor;
   }
 
-    public void setTaskStorage(ITaskStorage taskStorage) {
+  public void setTaskStorage(ITaskStorage taskStorage) {
         this.taskStorage = taskStorage;
     }
 }
